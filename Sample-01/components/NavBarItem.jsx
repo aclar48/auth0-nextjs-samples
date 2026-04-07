f982f0fbee09c8aeb592dc3eb65c2323087122b7
Alex <hahaiamusingtheinternet@gmail.com>
2026-04-07 15:19:51 -0400

    update vercel version

--- Sample-01/components/NavBarItem.jsx
+++ Sample-01/components/NavBarItem.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
 
 const NavBarItem = ({ children, href, className, icon, tabIndex, testId }) => {
   const pathname = usePathname();
   const activeClass = 'navbar-item-active';
   const activeClasses = className ? `${className} ${activeClass}` : activeClass;
-
+  console.log(icon)
   return (
     <span className="d-inline-flex align-items-center navbar-item">
-      {icon && <FontAwesomeIcon icon={icon} className="mr-3" />}
+      {icon && icon.length > 0 && <FontAwesomeIcon icon={`fa-solid fa-${icon}`} className="mr-3" />}
       <span className={pathname === href ? activeClasses : className} tabIndex={tabIndex} data-testid={testId}>
         {children}
       </span>
--- Sample-01/components/PageLink.jsx
+++ Sample-01/components/PageLink.jsx
@@ -5,12 +5,10 @@ import NavBarItem from './NavBarItem';
 
 const PageLink = ({ children, href, className, icon, tabIndex, testId }) => {
   return (
-    <Link legacyBehavior href={href}>
-      <a>
-        <NavBarItem href={href} className={className} icon={icon} tabIndex={tabIndex} testId={testId}>
-          {children}
-        </NavBarItem>
-      </a>
+    <Link href={href}>
+      <NavBarItem href={href} className={className} icon={icon} tabIndex={tabIndex} testId={testId}>
+        {children}
+      </NavBarItem>
     </Link>
   );
 };
